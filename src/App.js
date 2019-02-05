dde9d30fb0c196145d3c6b5c4b00a091edbb06e9
Stefano Corra <stefano.corra@hotmail.com>
2019-02-05 00:39:39 -0500

    components

--- src/App.js
+++ src/App.js
@@ -5,12 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 class App extends Component {
   render() {
     return (
-      <div className="container">
-          <div className="row">
-            <div className="col-6">column number one</div>
-            <div className="col-6"><span><i className="fas fa-home"></i></span></div>
-          </div>
-      </div>
+      <React.Fragment>
+
+      </React.Fragment>
     );
   }
 }
