57e007f023fefc6cff57cc8b1a752f9d01162809
Stefano Corra <stefano.corra@hotmail.com>
2019-02-07 23:38:39 -0500

    components

---
+++ src/component/Details.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Details extends Component {
+
+  render() {
+    return (
+      <div>
+        <h3>Details</h3>
+      </div>
+    );
+  }
+
+}
+
+export default Details;
