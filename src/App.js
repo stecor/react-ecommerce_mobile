d7d1a191a35924abcf6001d7e70bb66872a67daa
Stefano Corra <stefano.corra@hotmail.com>
2019-02-08 01:03:16 -0500

    router and links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+import {Switch, Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-
+        <Navbar/>
+        <Switch>
+          <Route exact path="/" component={ProductList}/>
+          <Route path="/details" component={Details}/>
+          <Route path="/cart" component={Cart}/>
+          <Route  component={Default}/>
+        </Switch>
       </React.Fragment>
     );
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../logo.svg';
+
+class Navbar extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm bg-primary navbar-dark ps-sm-5">
+        <Link to="/">
+          <img src={logo} alt="store" className="navbar-brand"/>
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+              <Link to="/" className="nav-link">
+                products
+              </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+
+}
+
+export default Navbar;
