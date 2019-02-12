703e93312f5b06a9727eacc4c9eaef82bb68e72c
Stefano Corra <stefano.corra@hotmail.com>
2019-02-11 22:39:16 -0500

    product

--- src/components/Product.js
+++ src/components/Product.js
@@ -18,6 +18,25 @@ class Product extends Component {
             <Link to="/details">
               <img src={img} alt="product" className="card-img-top"/>
             </Link>
+            <button className="cart-btn"
+                    disabled={ inCart? true : false }
+                    onClick={()=>{console.log('added to the cart');}}>
+                {
+                  (inCart)?
+                   (<p className="text-capitalize mb-0" disabled>{" "} in Cart</p>)
+                  :(<i className="fas fa-cart-plus" />)
+                }
+            </button>
+          </div>
+          {/* card footer */}
+          <div className="card-footer d-flex justify-content-between">
+            <p className="align-self-center mb-0">
+              {title}
+            </p>
+            <strong className="text-blue font-italic mb-0">
+              <span className="mr-1">$</span>
+              {price}
+            </strong>
           </div>
          </div>
       </ProductWrapper>
