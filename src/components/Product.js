327dcc375c2f048ce930c2b11a8654bca8434b56
Stefano Corra <stefano.corra@hotmail.com>
2019-02-12 23:33:19 -0500

    product img

--- src/components/Product.js
+++ src/components/Product.js
@@ -77,6 +77,30 @@ const ProductWrapper = styled.div`
   transform: scale(1.2);
 }
 
+.cart-btn{
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 0.2rem 0.4rem;
+  background: var(--lightBlue);
+  border: none;
+  color: var(--mainWhite);
+  font-size: 1.4rem;
+  border-radius:0.5rem 0 0 0;
+  transform:translate(100%,100%);
+  transition:all 1s linear;
+}
+
+.img-container:hover .cart-btn{
+    transform:translate(0,0);
+  }
+
+.cart-btn:hover{
+  color:var(--mainBlue);
+  cursor:pointer;
+}
+
+
 `;
 
 export default Product;
