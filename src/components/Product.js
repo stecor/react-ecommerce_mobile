a9b29a8d81962adb36d4503367c3af5710b1bff6
Stefano Corra <stefano.corra@hotmail.com>
2019-02-12 23:04:51 -0500

    product img

--- src/components/Product.js
+++ src/components/Product.js
@@ -14,7 +14,7 @@ class Product extends Component {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
          <div className="card">
-          <div className="img-containern p-5" onClick={()=>console.log('clicked img-container')}>
+          <div className="img-container p-5" onClick={()=>console.log('clicked img-container')}>
             <Link to="/details">
               <img src={img} alt="product" className="card-img-top"/>
             </Link>
@@ -44,6 +44,39 @@ class Product extends Component {
   }
 }
 
-const ProductWrapper = styled.div``;
+const ProductWrapper = styled.div`
+.card{
+  border-color:transparent;
+  transition: all 1s linear;
+}
+.card-footer{
+  background: transparent;
+  border-top: transparent;
+  transition: all 1s linear;
+}
+&:hover{
+  .card{
+    border-radio:5px;
+    border:0.04rem solid rgba(0,0,0,0.2);
+    box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.2);
+  }
+  .card-footer{
+    background:rgba(247,247,247);
+  }
+}
+.img-container{
+  position: relative;
+  overflow: hidden;
+}
+
+.card-img-top{
+  transition: all 1s linear;
+}
+
+.img-container:hover .card-img-top {
+  transform: scale(1.2);
+}
+
+`;
 
 export default Product;
