476f2ea642a6be69da43e03919fd9430863719b6
Stefano Corra <stefano.corra@hotmail.com>
2019-02-17 00:36:27 -0500

    add to cart

--- src/components/Product.js
+++ src/components/Product.js
@@ -13,7 +13,7 @@ class Product extends Component {
     const {id, title, img, price, inCart } = this.props.product;
 
     return (
-      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3 detail-zoom">
          <div className="card">
            <ProductConsumer>
              {(value)=>(
